d16907ea46e41461a0563e30de31188f6bb1f7a1
Gratis Mtuy <mtuygratis@gmail.com>
2021-01-28 19:31:49 +0300

    Parsing Route Data Manually & Adding Alert Dialogs

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,34 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_mall/pages/products_admin.dart';
 
-import 'product_create.dart';
-import 'product_list.dart';
+import '../product_manager.dart';
 
 class ProductsPage extends StatelessWidget {
+  final List<Map<String, String>> products;
+  final Function addProduct;
+  final Function deleteProduct;
+
+  ProductsPage(this.products, this.addProduct, this.deleteProduct);
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
-            drawer: Drawer(
-              child: Column(
-                children: <Widget>[
-                  AppBar(
-                    automaticallyImplyLeading: false,
-                    title: Text('Choose'),
-                  ),
-                  ListTile(
-                    title: Text('Manage Products'),
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/admin');
-                    },
-                  )
-                ],
-              ),
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text('Choose'),
+                ),
+                ListTile(
+                  title: Text('Manage Products'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/admin');
+                  },
+                )
+              ],
             ),
-            appBar: AppBar(
-              title: Text('Shopping Mall'),
-            ),
-            body: Center(child: Text('Manage your Products'))));
+          ),
+          appBar: AppBar(
+            title: Text('Shopping Mall'),
+          ),
+          body: ProductManager(products, addProduct, deleteProduct),
+        ));
   }
 }
