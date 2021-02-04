f46512c4f674f50570da9eaa28b146e962c4fe42
Gratis Mtuy <mtuygratis@gmail.com>
2021-02-04 19:27:49 +0300

    updated gradle distribution to 6.4.1
    previous distribution was throwing exception

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import './products.dart';
 
 class AuthPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return _AuthPageState();
   }
 }
@@ -20,40 +19,46 @@ class _AuthPageState extends State<AuthPage> {
           title: Text('Login'),
         ),
         body: Container(
-            margin: EdgeInsets.all(10.0),
-            child: ListView(children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (String value) {
-                  setState(() {
-                    _emailValue = double.parse(value);
-                  });
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    _passwordValue = value;
-                  });
-                },
-              ),
-              SwitchListTile(value: true, onChanged: (bool value) {}, title: Text('Accept Terms'),), 
-                SizedBox(
-                  height: 10.0,
-                ),
-                RaisedButton(
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                    child: Text('LOGIN'),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/');
-                    }),
-              ),
-            ])));
+          margin: EdgeInsets.all(10.0),
+          child: ListView(children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'E-mail'),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (String value) {
+                setState(() {
+                  _emailValue = value;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
+            ),
+            SwitchListTile(
+              value: _acceptTerms,
+              onChanged: (bool value) {
+                setState(() {
+                  _acceptTerms = value;
+                });
+              },
+              title: Text('Accept Terms'),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                child: Text('LOGIN'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/');
+                }),
+          ]),
+        ));
   }
 }
-
- 
