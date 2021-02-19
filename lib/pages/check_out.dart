6dc8512f041c6bb59d1de00b842e5f21c4c32adf
priscak <43216566+priscak@users.noreply.github.com>
2021-02-19 15:02:45 +0300

    Update check_out.dart
    Edited Checkout page

--- lib/pages/check_out.dart
+++ lib/pages/check_out.dart
@@ -4,6 +4,24 @@ class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+            appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.close,
+              size: 30.0,
+            ),
+            onPressed: () {
+              //Navigate to homepage
+//               Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                       builder: (context) => HomePage()));
+            },
+          ),
+        ],
+      ),
     body: Center(
       child: Text('Check Out'),
     ),
