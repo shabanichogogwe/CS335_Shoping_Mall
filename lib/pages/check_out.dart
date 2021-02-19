df727866bfb1a2268f4c5d77f5cb2f1bc8c18988
priscak <43216566+priscak@users.noreply.github.com>
2021-02-19 14:58:30 +0300

    Update check_out.dart
    Edited Checkout page

--- lib/pages/check_out.dart
+++ lib/pages/check_out.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class ProductListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Center(
+      child: Text('Check Out'),
+    ),
+    );
+  }
+}
